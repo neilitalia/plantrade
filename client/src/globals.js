89635bcdcaa1ced20db64fbff8421197f2a65d23
Neil Italia <neilitalia1998@gmail.com>
2021-10-14 14:30:09 -0500

    :rocket: fix deployment scripts

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:3001/api'
+export const API_BASE_URL = process.env.NODE_ENV === 'production'
+  ? `${window.location.origin}/api`
+  : 'http://localhost:3001/api'
