1183fe0c5dfa68f524a12b693a9f24bda3b2efbf
Neil Italia <neilitalia1998@gmail.com>
2023-01-19 22:16:57 -0600

    🔧 update db config

--- config/config.js
+++ config/config.js
@@ -4,15 +4,26 @@ module.exports = {
     database: "plantrade_development",
     dialect: "postgres",
     host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     database: "plantrade_test",
     dialect: "postgres",
     host: process.env.DB_HOST,
+    ssl: true,
   },
   production: {
     database: "plantrade_production",
     dialect: "postgres",
     host: process.env.DB_HOST,
+    ssl: true,
   },
 };
